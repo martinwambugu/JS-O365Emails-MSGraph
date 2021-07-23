833deb4e07ae91efbf02a3ddad006a380e5756fd
Martin Wambugu <martin.wambugu@gmail.com>
2021-07-23 20:53:14 +0300

    app  commit

---
+++ graph.js
@@ -0,0 +1,37 @@
+// Create an authentication provider
+const authProvider = {
+    getAccessToken: async () => {
+        // Call getToken in auth.js
+        return await getToken();
+    }
+};
+// Initialize the Graph client
+const graphClient = MicrosoftGraph.Client.initWithMiddleware({ authProvider });
+//Get user info from Graph
+async function getUser() {
+    ensureScope('user.read');
+    return await graphClient
+        .api('/me')
+        .select('id,displayName')
+        .get();
+}       
+
+async function getEmails(page) {
+    ensureScope('mail.read');
+
+    var pageSize = 10;
+  
+    var query = graphClient
+    .api('/me/messages')
+    .select ('subject,receivedDateTime')
+    .orderby('receivedDateTime desc')
+    .top(pageSize);
+
+    if (page && page >1){
+        query.skip(page * pageSize);
+    }
+
+    return await query.get();
+
+
+  }
---
+++ ui.js
@@ -0,0 +1,54 @@
+async function displayUI() {    
+  await signIn();
+
+  // Display info from user profile
+  const user = await getUser();
+  var userName = document.getElementById('userName');
+  userName.innerText = user.displayName;  
+
+  // Hide login button and initial UI
+  var signInButton = document.getElementById('signin');
+  signInButton.style = "display: none";
+  var content = document.getElementById('content');
+  content.style = "display: block";
+}
+
+var page = 1;
+async function displayEmail() {
+  var emails = await getEmails(page);
+  if (!emails || emails.value.length < 1) {
+    return;
+  }
+
+  var displayEmailButton = document.getElementById('displayEmail');
+  displayEmailButton.style = 'display: none';
+
+  var emailsUl = document.getElementById('emails');
+  // clear existing elements
+  emailsUl.innerHTML = '';
+  emails.value.forEach(email => {
+    var emailLi = document.createElement('li');
+    emailLi.innerText = `${email.subject} (${new Date(email.receivedDateTime).toLocaleString()})`;
+    emailsUl.appendChild(emailLi);
+  });
+
+  var pageNumber = document.getElementById('pageNumber');
+  pageNumber.style = 'display: inline';
+  pageNumber.innerHTML = `Page ${page}`;
+
+  // stop if there are no more emails to display
+  if (page === 1 && !emails['@odata.nextLink']) {
+    return;
+  }
+
+  var emailsPagingButtons = document.getElementById('emailsPaging');
+  emailsPagingButtons.style = 'display: block';
+
+  var previousEmailsButton = document.getElementById('emailsPagingPrevious');
+  // hide previous button on the first page
+  previousEmailsButton.style = `display: ${page === 1 ? 'none' : 'inline'}`;
+
+  var nextEmailsButton = document.getElementById('emailsPagingNext');
+  // hide next button on the last page
+  nextEmailsButton.style = `display: ${!emails['@odata.nextLink'] ? 'none' : 'inline'}`;
+}
